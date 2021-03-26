c98267528e162e7c391439133cd05882e3584c87
fmeyer <fabian.meyer@inovex.de>
2021-03-26 16:47:55 +0100

    init arrows

--- frontend/src/components/ProjectPage/ProjectPage.tsx
+++ frontend/src/components/ProjectPage/ProjectPage.tsx
@@ -1,44 +1,69 @@
-import React, { Component, useRef } from "react";
+import React, { Component, useRef, useState } from "react";
 import Xarrow from "react-xarrows";
 import { useWindowSize } from "../shared/useWindowSize";
+import "./ProjectPage.css"
 
-const boxStyle = {
-    border: "grey solid 2px",
-    borderRadius: "10px",
-    padding: "5px",
-    marginTop: "100px",
-
-};
 
 export default function ProjectPage() {
     useWindowSize();
+    const [isOpen, setIsOpen] = useState(false);
     const box1Ref = useRef(null); // nutzen, weil die Arrows erst am Ende gerendert werden --> das hier nutzen
-    const box2Ref = useRef(null); 
+    const box2Ref = useRef(null);
+    const box3Ref = useRef(null);
+    const box4Ref = useRef(null);
     return (
-        <div>
 
-            <div style={{ display: "flex", justifyContent: "space-between" }}>
-                <div ref={box1Ref} style={boxStyle}>
-                    hey
-        </div>
-                <p id="elem2" style={boxStyle}>
-                    hey2
-        </p>
-            </div>
-            <div style={{ display: "flex", justifyContent: "space-between" }}>
-                <div style={boxStyle}>
-                    hey
-      </div>
-                <div ref={box2Ref} style={boxStyle}>
-                    hey2
-      </div>
+        <div className="flex-column">
 
-            </div>
-            <Xarrow
-                start={box1Ref} //can be react ref
-                end={box2Ref} //or an id
-            />
+
+                <div className="motherBox">
+                    <div ref={box1Ref} className="box-style box-style-left">
+                        Step1
+                </div>
+                </div>
+                <div className="motherBox">
+                    <div className="box-style box-style-right" ref={box2Ref}>
+                        Step3
+                    </div>
+
+                </div>
+                <div className="motherBox">
+                    <div ref={box3Ref} className="box-style box-style-left" >
+                        Step4
+                    </div>
+                </div>
+                <div className="motherBox">
+                    <div ref={box4Ref} className="box-style box-style-right" >
+                        Step6
+                    </div>
+
+                </div>
+                <Xarrow
+                startAnchor="bottom"
+                    start={box1Ref} //can be react ref
+                    end={box2Ref} //or an id
+                    endAnchor="top"
+                />
+                <Xarrow
+                    startAnchor="bottom"
+                    start={box2Ref}
+                    end={box3Ref}
+                    endAnchor="top"
+                    headSize= {0}
+                    strokeWidth= {14}
+                    color="rgba(75, 109, 70, 0.5)"
+                    />
+                <Xarrow
+                startAnchor="bottom"
+                    start={box3Ref}
+                    end={box4Ref}
+                    endAnchor="top"
+                    dashness={{ strokeLen: 1, nonStrokeLen: 8, animation: 10 }}
+
+                    />
         </div>
+
+
 
     );
 }

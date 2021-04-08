ebbf87960f0e92e90d3516a34b9a9b34f76ac1b0
fmeyer <fabian.meyer@inovex.de>
2021-04-08 19:51:10 +0200

    fix curtain view to cover whole site

--- frontend/src/components/ProjectPage/ProjectPage.tsx
+++ frontend/src/components/ProjectPage/ProjectPage.tsx
@@ -20,159 +20,149 @@ import { Curtain } from "./curtain/curtain";
 import { Link } from "react-router-dom";
 
 export const ProjectPage = () => {
-  useWindowSize();
-  const [curtinIsOpen, setCurtinIsOpen] = useState(true);
-  const box1Ref = useRef(null); // nutzen, weil die Arrows erst am Ende gerendert werden --> das hier nutzen
-  const box2Ref = useRef(null);
-  const box3Ref = useRef(null);
-  const box4Ref = useRef(null);
-  const box5Ref = useRef(null);
-  const box6Ref = useRef(null);
-  const box7Ref = useRef(null);
-  return (
-    <div style={{ backgroundColor: BACKGROUND }}>
-      <Header to="/projectoverview" buttonName="BACK" />
-      {curtinIsOpen && (
-        <div
-          style={{
-            position: "absolute",
-            width: "100%",
-            height: "100%",
-            backgroundColor: "white",
-            zIndex: 9998,
-          }}
-        >
-          <Curtain update={setCurtinIsOpen} />
+    useWindowSize();
+    const [curtinIsOpen, setCurtinIsOpen] = useState(true);
+    const box1Ref = useRef(null); // nutzen, weil die Arrows erst am Ende gerendert werden --> das hier nutzen
+    const box2Ref = useRef(null);
+    const box3Ref = useRef(null);
+    const box4Ref = useRef(null);
+    const box5Ref = useRef(null);
+    const box6Ref = useRef(null);
+    const box7Ref = useRef(null);
+
+    return (
+        <div style={{backgroundColor: BACKGROUND}}>
+            <Header to="/projectoverview" buttonName="BACK"/>
+            {curtinIsOpen && (
+                <Curtain update={setCurtinIsOpen}/>
+            )}
+            <ScrollPage style={{display: curtinIsOpen ? 'none' : 'block'}}>
+                <div className="flex-column">
+                    <div className="motherBox">
+                        <ImgTxtItem
+                            src={am1}
+                            text="Animation"
+                            imgLeft={true}
+                            reference={box1Ref}
+                            to="/step1"
+                        ></ImgTxtItem>
+                    </div>
+                    <div className="motherBox">
+                        <ImgTxtItem
+                            src={am2}
+                            text="Pen + Paper"
+                            imgLeft={false}
+                            reference={box2Ref}
+                            to="/step2"
+                        ></ImgTxtItem>
+                    </div>
+                    <div className="motherBox">
+                        <ImgTxtItem
+                            src={am3}
+                            text="Video"
+                            imgLeft={true}
+                            reference={box3Ref}
+                            to="/step3"
+                        ></ImgTxtItem>
+                    </div>
+                    <div className="motherBox">
+                        <ImgTxtItem
+                            src={am4}
+                            text="Break"
+                            imgLeft={false}
+                            reference={box4Ref}
+                            to="/step4"
+                        ></ImgTxtItem>
+                    </div>
+
+                    <div className="motherBox">
+                        <ImgTxtItem
+                            src={am5}
+                            text="Discussion"
+                            imgLeft={true}
+                            reference={box5Ref}
+                            to="/step5"
+                        ></ImgTxtItem>
+                    </div>
+
+                    <div className="motherBox">
+                        <ImgTxtItem
+                            src={am6}
+                            text="Game"
+                            imgLeft={false}
+                            reference={box6Ref}
+                            to="/step6"
+                        ></ImgTxtItem>
+                    </div>
+
+                    <div className="motherBox">
+                        <ImgTxtItem
+                            src={am7}
+                            text="Outlook"
+                            imgLeft={true}
+                            reference={box7Ref}
+                            to="/step7"
+                        ></ImgTxtItem>
+                    </div>
+                    <Xarrow
+                        startAnchor="bottom"
+                        start={box1Ref} //can be react ref
+                        end={box2Ref} //or an id
+                        endAnchor="top"
+                        headSize={0}
+                        strokeWidth={8}
+                        color="rgba(75, 109, 70, 0.5)"
+                    />
+                    <Xarrow
+                        startAnchor="bottom"
+                        start={box2Ref}
+                        end={box3Ref}
+                        endAnchor="top"
+                        headSize={0}
+                        strokeWidth={8}
+                        color="rgba(75, 109, 70, 0.5)"
+                    />
+                    <Xarrow
+                        startAnchor="bottom"
+                        start={box3Ref}
+                        end={box4Ref}
+                        endAnchor="top"
+                        headSize={0}
+                        strokeWidth={8}
+                        color="rgba(75, 109, 70, 0.5)"
+                    />
+
+                    <Xarrow
+                        startAnchor="bottom"
+                        start={box4Ref} //can be react ref
+                        end={box5Ref} //or an id
+                        endAnchor="top"
+                        headSize={0}
+                        strokeWidth={8}
+                        color="rgba(75, 109, 70, 0.5)"
+                    />
+                    <Xarrow
+                        startAnchor="bottom"
+                        start={box5Ref}
+                        end={box6Ref}
+                        endAnchor="top"
+                        headSize={0}
+                        strokeWidth={8}
+                        color="rgba(75, 109, 70, 0.5)"
+                    />
+                    <Xarrow
+                        startAnchor="bottom"
+                        start={box6Ref}
+                        end={box7Ref}
+                        endAnchor="top"
+                        headSize={0}
+                        strokeWidth={8}
+                        color="rgba(75, 109, 70, 0.5)"
+                    />
+                </div>
+            </ScrollPage>
         </div>
-      )}
-      <ScrollPage>
-        <div className="flex-column">
-          <div className="motherBox">
-            <ImgTxtItem
-              src={am1}
-              text="Animation"
-              imgLeft={true}
-              reference={box1Ref}
-              to="/step1"
-            ></ImgTxtItem>
-          </div>
-          <div className="motherBox">
-            <ImgTxtItem
-              src={am2}
-              text="Pen + Paper"
-              imgLeft={false}
-              reference={box2Ref}
-              to="/step2"
-            ></ImgTxtItem>
-          </div>
-          <div className="motherBox">
-            <ImgTxtItem
-              src={am3}
-              text="Video"
-              imgLeft={true}
-              reference={box3Ref}
-              to="/step3"
-            ></ImgTxtItem>
-          </div>
-          <div className="motherBox">
-            <ImgTxtItem
-              src={am4}
-              text="Break"
-              imgLeft={false}
-              reference={box4Ref}
-              to="/step4"
-            ></ImgTxtItem>
-          </div>
-
-          <div className="motherBox">
-            <ImgTxtItem
-              src={am5}
-              text="Discussion"
-              imgLeft={true}
-              reference={box5Ref}
-              to="/step5"
-            ></ImgTxtItem>
-          </div>
-
-          <div className="motherBox">
-            <ImgTxtItem
-              src={am6}
-              text="Game"
-              imgLeft={false}
-              reference={box6Ref}
-              to="/step6"
-            ></ImgTxtItem>
-          </div>
-
-          <div className="motherBox">
-            <ImgTxtItem
-              src={am7}
-              text="Outlook"
-              imgLeft={true}
-              reference={box7Ref}
-              to="/step7"
-            ></ImgTxtItem>
-          </div>
-
-          <Xarrow
-            startAnchor="bottom"
-            start={box1Ref} //can be react ref
-            end={box2Ref} //or an id
-            endAnchor="top"
-            headSize={0}
-            strokeWidth={8}
-            color="rgba(75, 109, 70, 0.5)"
-          />
-          <Xarrow
-            startAnchor="bottom"
-            start={box2Ref}
-            end={box3Ref}
-            endAnchor="top"
-            headSize={0}
-            strokeWidth={8}
-            color="rgba(75, 109, 70, 0.5)"
-          />
-          <Xarrow
-            startAnchor="bottom"
-            start={box3Ref}
-            end={box4Ref}
-            endAnchor="top"
-            headSize={0}
-            strokeWidth={8}
-            color="rgba(75, 109, 70, 0.5)"
-          />
-
-          <Xarrow
-            startAnchor="bottom"
-            start={box4Ref} //can be react ref
-            end={box5Ref} //or an id
-            endAnchor="top"
-            headSize={0}
-            strokeWidth={8}
-            color="rgba(75, 109, 70, 0.5)"
-          />
-          <Xarrow
-            startAnchor="bottom"
-            start={box5Ref}
-            end={box6Ref}
-            endAnchor="top"
-            headSize={0}
-            strokeWidth={8}
-            color="rgba(75, 109, 70, 0.5)"
-          />
-          <Xarrow
-            startAnchor="bottom"
-            start={box6Ref}
-            end={box7Ref}
-            endAnchor="top"
-            headSize={0}
-            strokeWidth={8}
-            color="rgba(75, 109, 70, 0.5)"
-          />
-        </div>
-      </ScrollPage>
-    </div>
-  );
+    );
 };
 
 interface ImgTxtProps {

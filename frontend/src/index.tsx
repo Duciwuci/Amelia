28f32acf6f63d37699bcf0661219b8e6fb50e4a4
ajaus <jaus_alex@yahoo.de>
2021-04-11 14:20:56 +0200

    Added service worker code to frontend to enable PWA functionality

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+
 
 ReactDOM.render(
   <BrowserRouter>
@@ -10,3 +12,5 @@ ReactDOM.render(
   </BrowserRouter>,
   document.getElementById("root")
 );
+
+serviceWorkerRegistration.register();
